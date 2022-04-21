a56135b2878508ec555000343351a7491b22aaab
Italo Campos <italo.campos@buser.com.br>
2022-04-21 19:25:13 -0300

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import setuptools
 
-#with open("README.md", "r") as fh:
-#    long_description = fh.read()
 
 setuptools.setup(
     name='color-lib',
@@ -9,7 +7,6 @@ setuptools.setup(
     author='Italo Campos',
     author_email='italo.ramon.campos@gmail.com',
     description='A small Python 3.x lib to speed up the coloring usage throughout your Python projects.',
-    #long_description=long_description,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/italocampos/color',
