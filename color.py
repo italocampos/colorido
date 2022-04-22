9fb13a2c54c64ae6c72af83d3e51be7a040f4c78
Italo Campos <italo.campos@buser.com.br>
2022-04-21 22:53:29 -0300

    refactor: update the lib to use bg color

--- color.py
+++ color.py
@@ -9,189 +9,280 @@ funtions in a print context.
 @author: @italocampos
 '''
 
-import foreground, style as st
+import background as bg
+import foreground as fg
+import style as st
 
 
-def style(text, style = 'normal'):
-    ''' Returns the text styled according the provided parameter.
+def format(text: str, style='normal'):
+    '''It returns the text styled according the provided `style` parameter. It
+    returns the `text` with no changes if the provided `style` is invalid.
 
-    Possible styles:
+    Styles options:
         - 'd', 'dim' : the dim font style;
         - 'n', 'normal' : the normal font style;
         - 'b', 'bold' : the bold font style;
     The default style is 'normal'.
-    
+
     Parameters
     ----------
     text : str
         The text to be styled.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+
     Returns
     -------
     str
-        The styled text according the provided 'style' parameter.
+        The styled `text` according the provided `style` parameter.
     '''
 
     if style in ['d', 'dim']:
-        return f'{st.DIM}{text}{st.RESET}'.format(text = text)
-    elif style in ['n', 'normal']:
-        return f'{st.NORMAL}{text}{st.RESET}'.format(text = text)
-    elif style in ['b', 'bold']:
-        return f'{st.BRIGHT}{text}{st.RESET}'.format(text = text)
-    else:
-        raise(Exception('Unknown style.'))
+        return f'{st.DIM}{text}{st.RESET}'.format(text=text)
+    if style in ['n', 'normal']:
+        return f'{st.NORMAL}{text}{st.RESET}'.format(text=text)
+    if style in ['b', 'bold']:
+        return f'{st.BRIGHT}{text}{st.RESET}'.format(text=text)
+    return text
 
 
+def set_foreground(text: str, color: str, style: str, bg: str):
+    '''For internal use only. This function returns the `text` colored,
+    formatted and with the suitable background set.
 
-def red(text_, style_ = 'normal'):
-    ''' Returns the red colored text.
+    Parameters
+    ----------
+    text : str
+        The text to be colored with the provided `color`.
+    color: str
+        The color to be applied to the `text`.
+    style : str
+        The style to format the `text`.
+    bg : str
+        The color to be applied to `text` background.
+
+    Returns
+    -------
+    str
+        The colored, styled and background-colored `text` formatted according
+        with the provided parameters.
+    '''
+
+    colored_text = f'{color}' + '{text}'.format(
+        text=format(text=text, style=style)
+    )
+    return set_background(colored_text, color=bg)
+
+
+def set_background(text: str, color: str = None):
+    '''It returns a string with background color set according with the color
+    provide in `color` parameter. It returns the `text` with no changes if the
+    provided `color` is invalid.
+
+    Parameters
+    ----------
+    text : str
+        The text to have the background set.
+    color : str, optional
+        The string with the color name to be set in background of `color`.
+        Defatult is `None`.
+
+    Returns
+    -------
+    str
+        The `text` with background colored with the color provided in `color`.
+    '''
+
+    if color == 'black':
+        return f'{bg.BLACK}{text}{st.RESET}'
+    if color == 'red':
+        return f'{bg.RED}{text}{st.RESET}'
+    if color == 'green':
+        return f'{bg.GREEN}{text}{st.RESET}'
+    if color == 'yellow':
+        return f'{bg.YELLOW}{text}{st.RESET}'
+    if color == 'blue':
+        return f'{bg.BLUE}{text}{st.RESET}'
+    if color == 'magenta':
+        return f'{bg.MAGENTA}{text}{st.RESET}'
+    if color == 'cyan':
+        return f'{bg.CYAN}{text}{st.RESET}'
+    if color == 'white':
+        return f'{bg.WHITE}{text}{st.RESET}'
+    return text
+
+
+def red(text, style='normal', bg=None):
+    '''It returns the `text` colored with red color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the red color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.RED}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.RED, style=style, bg=bg)
 
 
-def black(text_, style_ = 'normal'):
-    ''' Returns the black colored text.
+def black(text, style='normal', bg=None):
+    '''It returns the `text` colored with black color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the black color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.BLACK}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.BLACK, style=style, bg=bg)
 
 
-def green(text_, style_ = 'normal'):
-    ''' Returns the green colored text.
+def green(text, style='normal', bg=None):
+    '''It returns the `text` colored with green color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the green color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.GREEN}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.GREEN, style=style, bg=bg)
 
 
-def yellow(text_, style_ = 'normal'):
-    ''' Returns the yellow colored text.
+def yellow(text, style='normal', bg=None):
+    '''It returns the `text` colored with yellow color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the yellow color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.YELLOW}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.YELLOW, style=style, bg=bg)
 
 
-def blue(text_, style_ = 'normal'):
-    ''' Returns the blue colored text.
+def blue(text, style='normal', bg=None):
+    '''It returns the `text` colored with blue color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the blue color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.BLUE}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.BLUE, style=style, bg=bg)
 
 
-def magenta(text_, style_ = 'normal'):
-    ''' Returns the magenta colored text.
+def magenta(text, style='normal', bg=None):
+    '''It returns the `text` colored with magenta color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the magenta color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.MAGENTA}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.MAGENTA, style=style, bg=bg)
 
 
-def cyan(text_, style_ = 'normal'):
-    ''' Returns the cyan colored text.
+def cyan(text, style='normal', bg=None):
+    '''It returns the `text` colored with cyan color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the cyan color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.CYAN}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.CYAN, style=style, bg=bg)
 
 
-def white(text_, style_ = 'normal'):
-    ''' Returns the white colored text.
+def white(text, style='normal', bg=None):
+    '''It returns the `text` colored with white color.
 
     Parameters
     ----------
     text : str
         The text to be colored with the white color.
     style : str, optional
-        The style to be used with the font.
-    
+        The style to be used with the font. Default is 'normal' style.
+    bg : str, optional
+        The color to be applied to `text` background. Default is no color
+        (`None`).
+
     Returns
     -------
     str
-        The styled and colored text according the provided 'style_' parameter.
+        The styled and colored text according the provided `style` parameter.
     '''
 
-    return f'{foreground.WHITE}' + '{text}'.format(text = style(text = text_, style = style_))
+    return set_foreground(text=text, color=fg.WHITE, style=style, bg=bg)
