2f1ec3888f777a04b47a31583f45028afe76385f
Italo Campos <italo.ramon.campos@gmail.com>
2022-06-17 23:46:15 -0300

    chore: clear setup.py file

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import setuptools
+# It's just for setuptools legacy support when installing with editable mode
+from setuptools import setup
 
-
-setuptools.setup(
-    name='colorido',
-    version='1.0.0',
-    author='Italo Campos',
-    author_email='italo.ramon.campos@gmail.com',
-    description='A small Python 3.x lib to speed up the coloring usage'
-    'throughout your Python projects.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/italocampos/colorido',
-    packages=setuptools.find_packages(),
-    install_requires=[],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.5',
-)
+setup()
